d4f893a260b471a6a72ec1791f7d0eba867487a5
Alinausa1212 <alinausa1212@gmail.com>
2023-11-04 11:11:56 -0400

    message

---
+++ src/test/java/CarFeatures.java
@@ -0,0 +1,8 @@
+public class CarFeatures {
+    public String color;
+    public String door;
+    String Doors;
+    String Color;
+    String Hp;
+    String model;
+}
---
+++ src/test/java/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        CarFeatures audi=new CarFeatures();
+        audi.color="black";
+        audi.Hp="5000";
+        audi.door="2";
+        audi.model="A98";
+    }
+}
